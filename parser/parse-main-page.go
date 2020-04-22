bc77e70e5606e462219ee9c9a4d945caec433dd3
pradip parmar <prince.soamedia@gmail.com>
2020-04-22 20:58:19 +0530

    time sleep

--- parser/parse-main-page.go
+++ parser/parse-main-page.go
@@ -3,6 +3,7 @@ package parser
 import (
 	"fmt"
 	"sync"
+	"time"
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/princeparmar/webscaping/outer"
@@ -23,27 +24,36 @@ func Collection(category string, pageno int) (bool, error) {
 		shouldContinue = true
 		for _, v := range el.Nodes {
 			wg.Add(1)
-			go func(node *html.Node) {
-				defer wg.Done()
-				url := FindAttr(*node, "href")
-				p, err := FetchProduct(url)
-				if err != nil {
-					fmt.Println("error in product fetch", err)
-					return
-				}
-
-				err = p.Save()
-				if err != nil {
-					fmt.Println("error in product save", err)
-				}
-			}(v)
+			go loadAndSaveProduct(v, &wg)
 		}
 	})
 
 	wg.Wait()
 	fmt.Println("downloaded collection", category, "pageno", pageno)
+	time.Sleep(10 * time.Second)
 	return shouldContinue, nil
 
+}
+
+func loadAndSaveProduct(node *html.Node, wg *sync.WaitGroup) {
+	defer wg.Done()
+	url := FindAttr(*node, "href")
+	p, err := FetchProduct(url)
+	if err != nil {
+		fmt.Println("error in product fetch", err)
+		time.Sleep(time.Second)
+		wg.Add(1)
+		loadAndSaveProduct(node, wg)
+		return
+	}
+
+	err = p.Save()
+	if err != nil {
+		fmt.Println("error in product save", err)
+		time.Sleep(time.Second)
+		wg.Add(1)
+		loadAndSaveProduct(node, wg)
+	}
 }
 
 func FullCollection(name string) error {
